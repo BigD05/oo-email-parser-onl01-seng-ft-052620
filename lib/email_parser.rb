3f48ede63fa63c79bc39508f999ae09406965424
kyle Dornan <kyledornan26@gmail.com>
2020-06-07 20:55:39 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -1,12 +1,25 @@
 class EmailAddressParser 
-  attr_accessor :parse
-  
-  
-  def initialize(parse)
-    @parse = parse
-  
-  end 
-  
+attr_accessor :emails 
+
+def initialize(emails)
+  @emails = emails
+end 
+
+def parse
+  puts splitEmail = emails.split.collect {|address| address.split(',')} 
+  returnArr = splitEmail.flatten.uniq
+  returnArr
+end
+
+end
+
+emails1 = "asd@qw.com, per@er.org, Zaza@sugar.net"
+parser1 = EmailParser.new(emails1)
+parser1.parse 
+
+emails2 = "asd@qw.com per@er.org per@er.org Zaza@sugar.net"
+parser2 = EmailParser.new(emails2)
+parser2.parse
 end 
   
   
